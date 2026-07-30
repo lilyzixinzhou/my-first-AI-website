d52aba2908824c0f6668322517a7b350b50f5c06
Zi Xin Zhou <zixinzhou@Lilys-MacBook-Pro.local>
2026-07-30 12:01:48 -0400

    commit 2

--- app.py
+++ app.py
@@ -2,27 +2,15 @@ import streamlit as st
 
 st.title("lily's super cool website")
 
-st.header("header")
-st.subheader("subheader")
-st.write("body text")
-
-count = 0
-
-if st.button("click me"):
-    count += 1
-st.write("count is ", count)
-
-name = st.text_input("what is your name?")
-letter_count = len(name)
-
-if st.button("submit"):
-    st.write(f"hello, {name}! welcome to ai lvl 2. there are {letter_count} letters in your name")
-
 with st.sidebar:
     st.header("settings")
+    name = st.text_input("what is your name?")
+    if st.button("submit"):
+        st.write(f"hello, {name}! welcome to ai level 2.")
     st.selectbox("select an option", ["one", "two"])
     st.multiselect("select a few options", ["one", "two"])
     st.slider("creativity", 0.1, 0.0, 0.5)
+
     with st.form("settings"):
         sources = st.multiselect("select a few options", ["one", "two", "three"])
         creativity = st.slider("creativity", 0.9, 0.8, 0.7)
@@ -33,4 +21,15 @@ with st.sidebar:
 left, right = st.columns(2)
 left.write("sources: 3")
 right.write("creativity: 2")
+
+with st.chat_message("user"):
+    st.write(f"hello, i am lily. welcome to ai level 2")
+with st.chat_message("assistant"):
+    st.write(f"hello {name}, i am billy!. welcome to ai level 2")
+
+prompt = st.chat_input("ask something here... ")
+
+if prompt:
+    with st.chat_message("user"):
+        st.write(prompt)
 
